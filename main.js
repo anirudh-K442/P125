5b7ed1e414eb724f00164fca55a9472a593de5c7
anirudh-K442 <79716755+anirudh-K442@users.noreply.github.com>
2022-03-13 12:25:43 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+song = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+song1 = "music.mp3"
+song2 = "music2.mp3"
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.position(500, 200);
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses)
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+}
+
+function preload() {
+    song = loadSound("music.mp3");
+    song = loadSound("music2.mp3");
+}
+
+function modelLoaded() {
+    console.log('PoseNet is Initialized');
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " +  leftWristX + "leftWristY = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY)
+    }
+}
